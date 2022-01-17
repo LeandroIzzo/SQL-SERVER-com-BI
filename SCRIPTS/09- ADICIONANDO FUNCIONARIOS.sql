a087fc652d798381c505298c1388c4a5a3eb54dc
Leandro A. Izzo <97754743+LeandroIzzo@users.noreply.github.com>
2022-01-17 11:25:31 -0300

    Add files via upload

---
+++ SCRIPTS/09- ADICIONANDO FUNCIONARIOS.sql
@@ -0,0 +1,13 @@
+USE LOJA_INSTRUMENTOS_OLTP
+GO
+
+/* ADICIONANDO FUNCIONARIOS */
+INSERT INTO FUNCIONARIOS VALUES('Marco Junior','M','1990-06-03','Marcojunior@gmail.com','São Jose','Osasco','SP','Gerente',5500.00)
+INSERT INTO FUNCIONARIOS VALUES('Thaiz Silva','F','1998-07-02','Thaizsilva@gmail.com','Rua Abacateiro','Osasco','SP','Caixa',1200.00)
+INSERT INTO FUNCIONARIOS VALUES('Leandro Angeli','M','2001-10-22','Leangel@gmail.com','Praça Abílio Frare','Osasco','SP','Jovem Aprendiz',850.00)
+INSERT INTO FUNCIONARIOS VALUES('Vayne Santos','F','1998-11-21','Vaynesan@gmail.com','Rua Adão','Barueri','SP','Recursos Humanos',2500.00)
+INSERT INTO FUNCIONARIOS VALUES('Ashe Rocha','F','1987-09-05','Asheroch@gmail.com','Rua Aguaí','Barueri','SP','Gerente Financeiro',4200.00)
+INSERT INTO FUNCIONARIOS VALUES('Olaf Maylon','M','1999-03-07','Olafmay@gmail.com','Rua Adelina','Carapicuiba','SP','Atendente',1500.00)
+INSERT INTO FUNCIONARIOS VALUES('Rute Lupi','F','1996-04-28','Rutelu@gmail.com','Rua Adventista','Carapicuiba','SP','Ajudante geral',1500.00)
+GO
+
